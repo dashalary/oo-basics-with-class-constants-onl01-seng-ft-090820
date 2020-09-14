18764f8b53272624a4d2e854f3901d0a4ce75226
Dasha Lary <dk2800@nyu.edu>
2020-09-14 18:26:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_accessor :color, :material, :condition
-  attr_reader :brand, :size
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand, 
   
   BRANDS = []
 
@@ -14,11 +14,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def size=(size)
-    @size = size
-    if @brand.size == 3
-      BRANDS << brand
-    end
-  end
   
 end
